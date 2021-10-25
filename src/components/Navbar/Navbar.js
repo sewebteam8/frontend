cdea10ba938c90dcde79ffe481ac855701d02880
rajendra kumar <rajendrakumar2770@gmail.com>
2021-10-25 22:12:08 +0530

    link resolved

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
                     </div>
 
                     <div className="col-3 navLinks">
-                            <NavLink to = "/" > YourBuddy </NavLink>
+                            <NavLink to = "/findbuddy" > YourBuddy </NavLink>
                     </div>
 
                     <div className="col-2 navLinks">
@@ -43,9 +43,7 @@ const Navbar = () => {
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                         </svg></div>
 
-                        <div className="col-2 navLinks">
-                             <Link to = "/findbuddy" > Find Your Buddy </Link>
-                        </div>
+                       
 
 
                     </div>
