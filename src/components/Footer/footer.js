e97d29f1746476e3526b22223167228557da50b6
rajendra kumar <rajendrakumar2770@gmail.com>
2021-12-01 16:21:37 +0530

    navbar and footer fixed

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,52 +1,43 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './footer.css'
-
-const Footer = () => {
-    return ( 
-        <React.Fragment>
-            <div className="container-fluid footer">
-                <div className="row">
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link">
-                        LINKS
-                    </div>
-                    <div className="col-sm-3 col-6 footer-link ">
-                        LINKS
-                    </div>
-                    
-                </div>
+const Footer = () =>
+    <footer className="page-footer font-small blue pt-4">
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+                <h5 className="text-uppercase">College Collab</h5>
+                <p>Collab - With specific focus on that section which generally from the rural setting. </p>
             </div>
-        </React.Fragment>
-     );
-}
- 
-export default Footer;
+
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+                <h5 className="text-uppercase">Links</h5>
+                <ul className="list-unstyled">
+                    <li><a href="#!">Link 1</a></li>
+                    <li><a href="#!">Link 2</a></li>
+                    <li><a href="#!">Link 3</a></li>
+                    <li><a href="#!">Link 4</a></li>
+                </ul>
+            </div>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+                <h5 className="text-uppercase">Links</h5>
+                <ul className="list-unstyled">
+                    <li><a href="#!">Link 1</a></li>
+                    <li><a href="#!">Link 2</a></li>
+                    <li><a href="#!">Link 3</a></li>
+                    <li><a href="#!">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div className="footer-copyright text-center py-3">© 2021 Copyright:
+        <a href="https://mdbootstrap.com/"> CollageCollab</a>
+    </div>
+
+</footer>
+
+export default Footer
